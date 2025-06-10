d0dcbd73ba87ea9175a7b9f124ab2103e4c6fc5e
maluroojas <marialuisarojas666@gmail.com>
2025-06-10 04:25:07 -0400

    feat: adicionar funções para importação de eventos e participantes

---
+++ comunidade_tech/data_import.py
@@ -0,0 +1,14 @@
+from dados import eventos
+from dados import participantes 
+
+def importar_eventos():
+    return eventos.eventos
+
+def importar_participantes():
+    return participantes.participantes
+
+if __name__ == "__main__":
+    eventos_importados = importar_eventos()
+    participantes_importados = importar_participantes()
+    print(f"{len(eventos_importados)} eventos importados")
+    print("f{len(participantes_importados)} participantes importados")
