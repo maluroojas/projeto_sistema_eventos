f6b90c64cd2ba0d9ad64426d42c5f8d2d89907f4
maluroojas <marialuisarojas666@gmail.com>
2025-06-17 21:29:06 -0400

    fix: corrigindo uso incorreto de f-string no print

--- comunidade_tech/data_import.py
+++ comunidade_tech/data_import.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     eventos_importados = importar_eventos()
     participantes_importados = importar_participantes()
     print(f"{len(eventos_importados)} eventos importados")
-    print("f{len(participantes_importados)} participantes importados")
+    print(f"{len(participantes_importados)} participantes importados")
